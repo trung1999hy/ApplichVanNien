bf74c0fc51d02a5d4cac91fe609fa14aa12fef67
quannd <quanhhbg990602@gmail.com>
2023-01-08 18:30:02 +0700

    implement ui header screens

--- app/src/main/java/com/example/universalcalendar/CalendarApplication.kt
+++ app/src/main/java/com/example/universalcalendar/CalendarApplication.kt
@@ -3,7 +3,14 @@ package com.example.universalcalendar
 import android.app.Application
 
 class CalendarApplication : Application() {
+
+    companion object {
+        var instance: CalendarApplication? = null
+        fun context() = instance!!.applicationContext!!
+    }
+
     override fun onCreate() {
         super.onCreate()
+        instance = this
     }
 }
--- app/src/main/java/com/example/universalcalendar/ui/base/BaseActivity.kt
+++ app/src/main/java/com/example/universalcalendar/ui/base/BaseActivity.kt
@@ -1,9 +1,14 @@
 package com.example.universalcalendar.ui.base
 
+import android.os.Build
 import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
+import com.example.universalcalendar.R
 
 abstract class BaseActivity<V: ViewDataBinding> : AppCompatActivity() {
 
@@ -16,6 +21,21 @@ abstract class BaseActivity<V: ViewDataBinding> : AppCompatActivity() {
         if (getLayoutId() != 0) {
             mViewDataBinding = DataBindingUtil.setContentView(this, getLayoutId())
             initView()
+        }
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+            window.statusBarColor = ContextCompat.getColor(this, R.color.colorTransparent)
+
+            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                    View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
         }
     }
 
